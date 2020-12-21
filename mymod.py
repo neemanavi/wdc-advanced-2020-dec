9b3c3a6d85d186554cb4688c916869aef00a99b1
Reuven Lerner <reuven@lerner.co.il>
2020-12-21 09:58:04 +0200

    gitautosync update 36

--- mymod.py
+++ mymod.py
@@ -11,6 +11,7 @@ def hello(name):
     return f'Hello, {name}!'
 
 
-for i in range(5):
-    print(f'Welcome to {__name__}!')
-    print(random.randint(0, 10))
+if __name__ == '__main__':
+    for i in range(5):
+        print(f'Welcome to {__name__}!')
+        print(random.randint(0, 10))
